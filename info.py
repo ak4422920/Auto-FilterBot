43f5c39d48a602fc3381475c2aaa0fe06612aa10
ak4422920 <ak4422920@gmail.com>
2025-02-11 21:19:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://ak:ak@cluster0.ftsd9
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'akmovies')
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002208313355'))
-QR_CODE = environ.get('QR_CODE', 'https://envs.sh/wam.jpg')
+QR_CODE = environ.get('QR_CODE', '')
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '-1002463332367').split()]
 
 #this vars is for when heroku or koyeb acc get banned, then change this vars as your file to link bot name
